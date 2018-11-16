f14a061b235a24f2594ae2db5b3322d0836be69c
likaifeng <1980942486@qq.com>
2018-11-16 15:46:07 +0800

    创建数据库连接

--- app.js
+++ app.js
@@ -6,20 +6,34 @@
  */
 // 导入第三方模块
 const express = require('express');
+const bodyParser = require('body-parser');
+
 // 导入自定义模块
 const index = require('./routes/index.js');
 const login = require('./routes/login.js');
 const article = require('./routes/atricle');
+
 // 创建服务器
 const app = express();
+
 // 静态文件的存放路径
 app.use(express.static(__dirname + '/public'));
+
+// 注册中间件
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({
+  extended: false
+})); // for parsing application/x-www-form-urlencoded
+
 // 设置模板引擎和模板存放的文件夹
 app.set("view engine", "ejs");
 app.set('views', __dirname + '/views');;
+
 // 创建路由
 app.get('/', index.render);
 app.get('/login', login.render);
+app.post('login', login.login);
 app.get('/article', article.render);
+
 // 启动服务器
 app.listen('3000', () => console.log('服务器启动成功...'))
--- routes/login.js
+++ routes/login.js
@@ -1,3 +1,11 @@
+// 导入自定义模块
+const db = require('../lib/db.js');
+
+// 渲染登录页面
 exports.render = (req, res) => {
   res.render('login', {})
+}
+exports.login = (req, res) => {
+  const data = req.bady;
+  console.log(data)
 }
